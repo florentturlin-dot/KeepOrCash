fbebf3c7cc008cefaddef9a91db8f0f8aa296c13
florentturlin-dot <florent.turlin@gmail.com>
2025-09-05 09:56:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
-export default {
-  server: { port: 5173 }
-};
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 5173
+  },
+  build: {
+    outDir: 'dist'
+  }
+});
